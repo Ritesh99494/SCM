14c3b8dfc159439205fa93180ff08627461b91e4
Ritesh <riteshpande097@gmail.com>
2025-04-04 11:54:41 +0530

    initialize project structure with Tailwind CSS integration and basic configurations

---
+++ SCM/src/main/java/com/Royal/SCM/Controllers/pageController.java
@@ -0,0 +1,22 @@
+package com.Royal.SCM.Controllers;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class pageController {
+    @RequestMapping("/Ritesh")
+ public String home(Model model) {
+       // Add any attributes to the model if needed
+       // model.addAttribute("attributeName", attributeValue);
+       // Return the name of the view (home.html) to be rendered
+       // The view resolver will resolve this to the actual HTML file in the templates directory
+       System.out.println("home page");
+       model.addAttribute("name", "Ritesh");
+       model.addAttribute("age", 25);
+              return "home";
+ }
+}
