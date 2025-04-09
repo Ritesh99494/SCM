303d181e67e47947563dd6d8fb743152cb93d1f9
Ritesh <riteshpande097@gmail.com>
2025-04-09 19:01:59 +0530

    Refactor UserService interface for improved user management functionality

--- SCM/src/main/java/com/Royal/SCM/Controllers/pageController.java
+++ SCM/src/main/java/com/Royal/SCM/Controllers/pageController.java
@@ -2,6 +2,7 @@ package com.Royal.SCM.Controllers;
 
 
 
+import org.apache.logging.log4j.message.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.Royal.SCM.entities.User;
 import com.Royal.SCM.servises.UserService;
 
 
--- SCM/src/main/java/com/Royal/SCM/servises/UserService.java
+++ SCM/src/main/java/com/Royal/SCM/servises/UserService.java
@@ -1,5 +1,27 @@
 package com.Royal.SCM.servises;
+import java.util.List;
+import java.util.Optional;
+
+import com.Royal.SCM.entities.User;
+
+
 
 public interface UserService {
+
+    User saveUser(User user);
+
+    Optional<User> getUserById(String id);
+
+    Optional<User> updateUser(User user);
+
+    void deleteUser(String id);
+
+    boolean isUserExist(String userId);
+
+    boolean isUserExistByEmail(String email);
+
+    List<User> getAllUsers();
+
+    User getUserByEmail(String email);
 
 }
