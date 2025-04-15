42fc890fa57bc41fb1f666a8a050eabb2b554cf2
Ritesh99494 <rp2511509@gmail.com>
2025-04-15 09:52:02 +0530

    Refactor user registration process to use setter methods for User entity; add 'enabled' field initialization and update User entity to include email and phone verification flags.

--- SCM/src/main/java/com/Royal/SCM/Controllers/pageController.java
+++ SCM/src/main/java/com/Royal/SCM/Controllers/pageController.java
@@ -66,16 +66,17 @@ public String register(Model model) {
         // userservice
 
         // UserForm--> User
-        User user = User.builder()
-         .name(userForm.getName())
-         .email(userForm.getEmail())
-         .password(userForm.getPassword())
-         .about(userForm.getAbout())
-        .phoneNumber(userForm.getPhoneNumber())
-         .profilePic(
-         "https://www.learncodewithdurgesh.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fdurgesh_sir.35c6cb78.webp&w=1920&q=75")
-         .build();
-           User savedUser = userService.saveUser(user);
+        User user = new User();
+        user.setName(userForm.getName());
+        user.setEmail(userForm.getEmail());
+        user.setPassword(userForm.getPassword());
+        user.setAbout(userForm.getAbout());
+        user.setPhoneNumber(userForm.getPhoneNumber());
+        user.setEnabled(false);
+        user.setProfilePic(
+                "https://www.learncodewithdurgesh.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fdurgesh_sir.35c6cb78.webp&w=1920&q=75");
+
+        User savedUser = userService.saveUser(user);
         System.out.println("User saved: " + savedUser);
 
         // redirectto login page
