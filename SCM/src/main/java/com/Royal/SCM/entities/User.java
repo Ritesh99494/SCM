2893b0a8ef412e58132d38a0b1632f1f0224e556
Ritesh99494 <rp2511509@gmail.com>
2025-04-21 17:00:21 +0530

    Implement UserDetails interface in User entity; add role management and account status methods.

--- SCM/src/main/java/com/Royal/SCM/entities/User.java
+++ SCM/src/main/java/com/Royal/SCM/entities/User.java
@@ -1,10 +1,17 @@
 package com.Royal.SCM.entities;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -12,6 +19,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -25,7 +33,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class User {
+public class User implements UserDetails{
     @Id
     private String userId;
     @Column(name = "user_name", nullable = false)
@@ -38,6 +46,7 @@ public class User {
     @Column(length = 1000)
     private String profilePic;
     private String phoneNumber;
+    @Getter(value = AccessLevel.NONE)
     //information
     private boolean enabled = false;
     private boolean emailVerified = false;
@@ -48,5 +57,44 @@ public class User {
     private String providerUserId;
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
     private List<Contact> contacts = new ArrayList<>();
+
+     @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roleList = new ArrayList<>();
+
+    private String emailToken;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+       // list of roles[USER,ADMIN]
+        // Collection of SimpGrantedAuthority[roles{ADMIN,USER}]
+        Collection<SimpleGrantedAuthority> roles = roleList.stream().map(role -> new SimpleGrantedAuthority(role))
+                .collect(Collectors.toList());
+        return roles;
+    }
+    // email id hai wahi hamare username hai
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
 
 }
