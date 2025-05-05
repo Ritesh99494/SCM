0b615ddf636b1b98eb7565efca71c4b2bf3266e9
Ritesh99494 <rp2511509@gmail.com>
2025-05-05 22:39:45 +0530

    Implement custom authentication failure handling for disabled users

--- SCM/src/main/java/com/Royal/SCM/config/AuthFailtureHandler.java
+++ SCM/src/main/java/com/Royal/SCM/config/AuthFailtureHandler.java
@@ -2,20 +2,41 @@ package com.Royal.SCM.config;
 
 import java.io.IOException;
 
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import com.Royal.SCM.helpers.Message;
+import com.Royal.SCM.helpers.MessageType;
+
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 public class AuthFailtureHandler  implements AuthenticationFailureHandler{
 
-    @Override
+     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
             AuthenticationException exception) throws IOException, ServletException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'onAuthenticationFailure'");
+
+        if (exception instanceof DisabledException) {
+
+            // user is disabled
+            HttpSession session = request.getSession();
+            session.setAttribute("message",
+                    Message.builder()
+                            .content("User is disabled, Email with  varification link is sent on your email id !!")
+                            .type(MessageType.red).build());
+
+            response.sendRedirect("/login");
+        } else {
+            response.sendRedirect("/login?error=true");
+            // request.getRequestDispatcher("/login").forward(request, response);
+
+        }
+
     }
 
 }
