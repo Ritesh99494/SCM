d8e4c69e41fedbb596c78a368bcbdee0e4906cdb
Ritesh99494 <rp2511509@gmail.com>
2025-05-17 09:41:12 +0530

    Enhance SecurityConfig to implement custom form login and OAuth2 login handling

--- SCM/src/main/java/com/Royal/SCM/config/SecurityConfig.java
+++ SCM/src/main/java/com/Royal/SCM/config/SecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -71,12 +71,35 @@ public class SecurityConfig {
             authorize.requestMatchers("/user/**").authenticated();
             authorize.anyRequest().permitAll();
         });
-        return null;
-
         // form default login
         // agar hame kuch bhi change karna hua to hama yaha ayenge: form login se
         // related
-        
+        httpSecurity.formLogin(formLogin -> {
+
+            //
+            formLogin.loginPage("/login");
+            formLogin.loginProcessingUrl("/authenticate");
+            formLogin.successForwardUrl("/user/profile");
+            // formLogin.failureForwardUrl("/login?error=true");
+            // formLogin.defaultSuccessUrl("/home");
+            formLogin.usernameParameter("email");
+            formLogin.passwordParameter("password");
+            formLogin.failureHandler(authFailtureHandler);
+
+        });
+         httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        // oauth configurations
+
+        httpSecurity.oauth2Login(oauth -> {
+            oauth.loginPage("/login");
+            oauth.successHandler(handler);
+        });
+
+        httpSecurity.logout(logoutForm -> {
+            logoutForm.logoutUrl("/do-logout");
+            logoutForm.logoutSuccessUrl("/login?logout=true");
+        });
+        return httpSecurity.build();
 
     }
  @Bean
