2da07f5d726b3e905cc6aa80042df9fa6726cec6
Ritesh99494 <rp2511509@gmail.com>
2025-05-17 09:55:12 +0530

    fix bug of AuthFailureHandler for Spring context integration

--- SCM/src/main/java/com/Royal/SCM/config/AuthFailtureHandler.java
+++ SCM/src/main/java/com/Royal/SCM/config/AuthFailtureHandler.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
 
 import com.Royal.SCM.helpers.Message;
 import com.Royal.SCM.helpers.MessageType;
@@ -15,6 +16,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
+@Component
 public class AuthFailtureHandler  implements AuthenticationFailureHandler{
 
      @Override
