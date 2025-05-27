9f80c31a4fe552d3b436bfc60d93a55dd11c95ef
Ritesh99494 <rp2511509@gmail.com>
2025-05-27 09:27:57 +0530

    Add ContactController and add_contact template for managing user contacts

---
+++ SCM/src/main/java/com/Royal/SCM/Controllers/ContactController.java
@@ -0,0 +1,16 @@
+package com.Royal.SCM.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user/contacts")
+public class ContactController {
+    public String addContactView() {
+       
+        return "user/add_contact";
+    } 
+        
+    
+}
+
