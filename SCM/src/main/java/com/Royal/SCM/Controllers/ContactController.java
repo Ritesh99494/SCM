2b5c0e26c04eb384efd5b5873d1a97e27ccda497
Ritesh99494 <rp2511509@gmail.com>
2025-05-27 10:27:10 +0530

    Add ContactForm class and update ContactController to handle contact creation

--- SCM/src/main/java/com/Royal/SCM/Controllers/ContactController.java
+++ SCM/src/main/java/com/Royal/SCM/Controllers/ContactController.java
@@ -1,15 +1,23 @@
 package com.Royal.SCM.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.Royal.SCM.forms.ContactForm;
 
 @Controller
 @RequestMapping("/user/contacts")
 public class ContactController {
-    public String addContactView() {
-       
+    @RequestMapping("/add")
+    // add contact page: handler
+    public String addContactView(Model model) {
+        ContactForm contactForm = new ContactForm();
+
+        contactForm.setFavorite(true);
+        model.addAttribute("contactForm", contactForm);
         return "user/add_contact";
-    } 
+    }
         
     
 }
