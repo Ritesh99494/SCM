374e8cade913626644e676126ca15dba443a4906
Ritesh99494 <rp2511509@gmail.com>
2025-05-27 14:42:04 +0530

    Add ContactRepo and ContactService interfaces for contact management functionality

--- SCM/src/main/java/com/Royal/SCM/Controllers/ContactController.java
+++ SCM/src/main/java/com/Royal/SCM/Controllers/ContactController.java
@@ -1,14 +1,28 @@
 package com.Royal.SCM.Controllers;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.slf4j.Logger;
+
 import com.Royal.SCM.forms.ContactForm;
+import com.Royal.SCM.helpers.Message;
+import com.Royal.SCM.helpers.MessageType;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/user/contacts")
 public class ContactController {
+    private Logger logger = org.slf4j.LoggerFactory.getLogger(ContactController.class);
+
+
+
     @RequestMapping("/add")
     // add contact page: handler
     public String addContactView(Model model) {
@@ -18,7 +32,25 @@ public class ContactController {
         model.addAttribute("contactForm", contactForm);
         return "user/add_contact";
     }
+    // @RequestMapping(value = "/add", method = RequestMethod.POST)
+    //  public String saveContact(@Valid @ModelAttribute ContactForm contactForm, BindingResult result,
+    //         Authentication authentication, HttpSession session) {
+
+    //     // process the form data
+
+    //     // 1 validate form
+
+    //     if (result.hasErrors()) {
+
+    //         result.getAllErrors().forEach(error -> logger.info(error.toString()));
+
+    //         session.setAttribute("message", Message.builder()
+    //                 .content("Please correct the following errors")
+    //                 .type(MessageType.red)
+    //                 .build());
+    //         return "user/add_contact";
+    //     }
         
-    
+    // }
 }
 
