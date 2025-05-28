fe997ebd4d08b0c2f352134de5b291169ccf0d0b
Ritesh99494 <rp2511509@gmail.com>
2025-05-28 21:54:51 +0530

    Implement ImageServiceImpl for image upload and URL retrieval functionality

---
+++ SCM/src/main/java/com/Royal/SCM/servises/impl/ImageServiceImpl.java
@@ -0,0 +1,18 @@
+package com.Royal.SCM.servises.impl;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.Royal.SCM.servises.ImageService;
+
+public class ImageServiceImpl implements ImageService {
+     public String uploadImage(MultipartFile contactImage, String filename) {
+
+        return null;
+     }
+      public String getUrlFromPublicId(String publicId) {
+        return null;
+    }
+
+
+
+}
