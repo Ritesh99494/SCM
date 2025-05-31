8303521832a8c45e3393a830ee24c18b13e83a50
Ritesh99494 <rp2511509@gmail.com>
2025-05-31 16:31:33 +0530

    Implement file validation logic in FileValidator and enable ValidFile annotation

--- SCM/src/main/java/validators/ValidFile.java
+++ SCM/src/main/java/validators/ValidFile.java
@@ -9,7 +9,7 @@ import jakarta.validation.Payload;
 @Target({ ElementType.FIELD, ElementType.METHOD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR,
         ElementType.PARAMETER })
 @Retention(RetentionPolicy.RUNTIME)
-//@Constraint(validatedBy = FileValidator.class)
+@Constraint(validatedBy = FileValidator.class)
 public @interface ValidFile {
     String message() default "Invalid file";
 
