576d29f372d678243030cc92a1644fa2e37d2e76
Ritesh99494 <rp2511509@gmail.com>
2025-06-16 12:29:04 +0530

    feat: implement email sending functionality in EmailServiceImpl and update interface methods

--- SCM/src/main/java/com/Royal/SCM/servises/impl/EmailServiceImpl.java
+++ SCM/src/main/java/com/Royal/SCM/servises/impl/EmailServiceImpl.java
@@ -1,15 +1,29 @@
 package com.Royal.SCM.servises.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.Royal.SCM.servises.EmailService;
+
+
 @Service
 public class EmailServiceImpl implements EmailService {
 
+    @Autowired
+    private JavaMailSender eMailSender;
+
+    @Value("${spring.mail.properties.domain_name}")
+    private String domainName;
+
     @Override
     public void sendEmail(String to, String subject, String body) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sendEmail'");
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        message.setFrom(domainName);
+        eMailSender.send(message);
+
     }
 
     @Override
@@ -19,7 +33,7 @@ public class EmailServiceImpl implements EmailService {
     }
 
     @Override
-    public void sendEmailWithAttachment(String to, String subject, String body, String attachmentPath) {
+    public void sendEmailWithAttachment() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'sendEmailWithAttachment'");
     }
