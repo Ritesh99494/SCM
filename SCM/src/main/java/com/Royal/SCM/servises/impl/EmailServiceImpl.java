101320d18c69179953b5fe29be378a37fc78ed19
Ritesh99494 <rp2511509@gmail.com>
2025-06-16 12:30:27 +0530

    refactor: reorganize imports in EmailServiceImpl for better readability

--- SCM/src/main/java/com/Royal/SCM/servises/impl/EmailServiceImpl.java
+++ SCM/src/main/java/com/Royal/SCM/servises/impl/EmailServiceImpl.java
@@ -1,8 +1,13 @@
 package com.Royal.SCM.servises.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import com.Royal.SCM.servises.EmailService;
+
+
 
 
 @Service
