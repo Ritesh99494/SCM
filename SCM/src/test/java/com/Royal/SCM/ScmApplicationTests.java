9a7e4feb7b6fd966758c97b0cad21c15e038f84d
Ritesh99494 <rp2511509@gmail.com>
2025-06-17 22:26:59 +0530

    feat: add domain name configuration for email service in application properties

--- SCM/src/test/java/com/Royal/SCM/ScmApplicationTests.java
+++ SCM/src/test/java/com/Royal/SCM/ScmApplicationTests.java
@@ -1,22 +1,19 @@
 package com.Royal.SCM;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.Royal.SCM.servises.EmailService;
 
 @SpringBootTest
 class ScmApplicationTests {
 
-	private EmailService service;
 
-	@Test
-	void contextLoads() {
-	}
-	   @Test
-		void sendEmailTest() {
-		service.sendEmail("batchlcwd@gmail.com", "Just managing the emails",
-				"this is scm project working on email service");
-	}
+
+	// @Test
+	// void contextLoads() {
+	// }
+	//    @Test
+	// 	void sendEmailTest() {
+	// 	service.sendEmail("batchlcwd@gmail.com", "Just managing the emails",
+	// 			"this is scm project working on email service");
+	// }
 //
 }
