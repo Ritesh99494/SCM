aa983dfdd8fa68fe929c469cfd5032fffa57068e
Ritesh99494 <rp2511509@gmail.com>
2025-06-25 18:27:09 +0530

    feat: add EmailResponse and IntentRequest DTOs for email handling

---
+++ SCM/src/main/java/dto/EmailResponse.java
@@ -0,0 +1,12 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailResponse {
+   private String message;
+}
