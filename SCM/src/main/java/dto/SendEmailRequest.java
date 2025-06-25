9e971567d80ed72a500c18fc089745f11c081fb9
Ritesh99494 <rp2511509@gmail.com>
2025-06-25 19:09:49 +0530

    feat: implement EmailController for sending emails with request DTO

---
+++ SCM/src/main/java/dto/SendEmailRequest.java
@@ -0,0 +1,15 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data  // generates getters/setters, toString(), equals(), hashCode()
+@NoArgsConstructor // required by Spring when mapping JSON to Java object
+@AllArgsConstructor
+public class SendEmailRequest {
+
+    private String to;
+    private String subject;
+    private String body;
+}
